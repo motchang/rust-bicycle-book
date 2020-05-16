7c5b328251274aa2d6207d97c4e5c144bf985a62
motchang <motchang@gmail.com>
2020-05-16 17:40:54 +0900

    3-3-6

--- bitonic-sorter/src/first.rs
+++ bitonic-sorter/src/first.rs
@@ -9,17 +9,28 @@ pub fn sort(x: &mut [u32], up: bool) {
     // unimplemented!();
 
     if x.len() > 1 {
-	let mid_point = x.len() / 2;
-	sort(&mut x[..mid_point], true);
-	sort(&mut x[mid_point..], false);
-	sub_sort(x, up);
+        let mid_point = x.len() / 2;
+        sort(&mut x[..mid_point], true);
+        sort(&mut x[mid_point..], false);
+        sub_sort(x, up);
     }
 }
 
 fn sub_sort(x: &mut [u32], up: bool) {
-    unimplemented!();
+    if x.len() > 1 {
+        compare_and_swap(x, up);
+        let mid_point = x.len() / 2;
+        sub_sort(&mut x[..mid_point], up);
+        sub_sort(&mut x[mid_point..], up);
+    }
 }
 
 fn compare_and_swap(x: &mut [u32], up: bool) {
-    unimplemented!();
+    let mid_point = x.len() / 2;
+    for i in 0..mid_point {
+        if (x[mid_point + 1] < x[i]) == up {
+            // 要素を交換する
+            x.swap(i, mid_point + 1);
+        }
+    }
 }
