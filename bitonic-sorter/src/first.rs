6f84c9eb36975260d7f8e8e80a09bfafef12534f
motchang <motchang@gmail.com>
2020-05-16 17:28:32 +0900

    3-3-5

--- bitonic-sorter/src/first.rs
+++ bitonic-sorter/src/first.rs
@@ -6,7 +6,14 @@
 //   [u32] 型はu32のスライス（現時点ではスライスは1次元の配列と考えてよい）
 pub fn sort(x: &mut [u32], up: bool) {
     // 未実装の意味。コンパイルは通るが、実行すると panic する
-    unimplemented!();
+    // unimplemented!();
+
+    if x.len() > 1 {
+	let mid_point = x.len() / 2;
+	sort(&mut x[..mid_point], true);
+	sort(&mut x[mid_point..], false);
+	sub_sort(x, up);
+    }
 }
 
 fn sub_sort(x: &mut [u32], up: bool) {
