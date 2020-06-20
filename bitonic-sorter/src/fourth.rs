6a806cdac4763d301c54b3c6ace984da586ada52
motchang <motchang@gmail.com>
2020-06-20 17:24:18 +0900

    3-6-6

--- bitonic-sorter/src/fourth.rs
+++ bitonic-sorter/src/fourth.rs
@@ -58,14 +58,14 @@ where T: Send,
             // error[E0277]: `F` cannot be shared between threads safely
             // error[E0277]: `T` cannot be sent between threads safely
 
-            rayon::join(|| do_sort(first, forward, comparator),
+            rayon::join(|| do_sort(first, true, comparator),
                         || do_sort(second, false, comparator));
         } else {
             // x をバイトニックソートする
             // 第二引数が true のときは comparator で示される順序でソート
-            do_sort(&mut x[..mid_point], true, comparator);
+            do_sort(first, true, comparator);
             // 第二引数が false のときは comparator とは逆順でソート
-            do_sort(&mut x[mid_point..], false, comparator);
+            do_sort(second, false, comparator);
         }
 
         sub_sort(x, forward, comparator);
@@ -73,13 +73,21 @@ where T: Send,
 }
 
 fn sub_sort<T, F>(x: &mut [T], forward: bool, comparator: &F)
-    where F: Fn(&T, &T) -> Ordering
+where T: Send,
+      F: Sync + Fn(&T, &T) -> Ordering
 {
     if x.len() > 1 {
         compare_and_swap(x, forward, comparator);
         let mid_point = x.len() / 2;
-        sub_sort(&mut x[..mid_point], forward, comparator);
-        sub_sort(&mut x[mid_point..], forward, comparator);
+        let (first, second) = x.split_at_mut(mid_point);
+
+        if mid_point >= PARALLEL_THRESHOLD {
+            rayon::join(|| sub_sort(first, forward, comparator),
+                        || sub_sort(second, forward, comparator));
+        } else {
+            sub_sort(first, forward, comparator);
+            sub_sort(second, forward, comparator);
+        }
     }
 }
 
